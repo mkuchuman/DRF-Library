23b224830518c0512e95c684453d9567c08ed594
mkuchuman <mkuchuman1@gmail.com>
2024-09-02 19:04:22 +0200

    included user.urls to books_service.urls

--- books_service/urls.py
+++ books_service/urls.py
@@ -17,11 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/books/", include("books.urls")),
-    path('api/user/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/user/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("api/books/", include("books.urls", namespace="books")),
+    path("api/users/", include("user.urls", namespace="user")),
 ]
