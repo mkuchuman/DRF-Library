4b934d56b9bc18ce061ad2c7089e1648ee810113
mkuchuman <mkuchuman1@gmail.com>
2024-09-02 18:26:23 +0200

    Added new endpoint for obtaining token and refreshing it

--- books_service/urls.py
+++ books_service/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("books/", include("books.urls"))
+    path("api/books/", include("books.urls")),
+    path('api/user/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/user/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
