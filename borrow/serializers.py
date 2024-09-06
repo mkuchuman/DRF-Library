4437ef23d8600c052271fafbcee11215e31b1ded
mkuchuman <mkuchuman1@gmail.com>
2024-09-06 16:43:47 +0200

    Created validate create update methods for Borrow Serializer

--- borrow/serializers.py
+++ borrow/serializers.py
@@ -1,11 +1,17 @@
+from django.db import transaction
 from rest_framework import serializers
 
+from books.models import Book
 from books.serializers import BookSerializer
 from borrow.models import Borrow
+from user.serializers import UserSerializer
 
 
-class BorrowReadSerializer(serializers.ModelSerializer):
-    book = BookSerializer(read_only=True)
+class BorrowSerializer(serializers.ModelSerializer):
+    book = serializers.PrimaryKeyRelatedField(
+        queryset=Book.objects.all()
+    )
+    user = UserSerializer(read_only=True)
 
     class Meta:
         model = Borrow
@@ -17,3 +23,29 @@ class BorrowReadSerializer(serializers.ModelSerializer):
             "book",
             "user",
         )
+
+    def validate_book(self, book):
+        if self.instance is None and book.inventory <= 0:
+            raise serializers.ValidationError("There is no book to borrow")
+        return book
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            user = self.context["request"].user
+            book = validated_data["book"]
+
+            book.inventory -= 1
+            book.save()
+
+            borrow = Borrow.objects.create(user=user, **validated_data)
+            return borrow
+
+    def update(self, instance, validated_data):
+        if (
+            "actual_return_date" in validated_data
+            and validated_data["actual_return_date"]
+        ):
+            instance.book.inventory += 1
+            instance.book.save()
+
+        return super().update(instance, validated_data)
