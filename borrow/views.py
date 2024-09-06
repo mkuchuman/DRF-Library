6a6fd4233496d2cba147a5ea91977f1aaded1477
mkuchuman <mkuchuman1@gmail.com>
2024-09-06 16:48:58 +0200

    The get_queryset method has been rewritten and now it is possible to filter borrowings

--- borrow/views.py
+++ borrow/views.py
@@ -1,11 +1,40 @@
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from borrow.models import Borrow
-from borrow.serializers import BorrowReadSerializer
+from borrow.serializers import BorrowSerializer
 
 
 # Create your views here.
-class BorrowViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, GenericViewSet):
-    queryset = Borrow.objects.select_related("book", "user").all()
-    serializer_class = BorrowReadSerializer
+class BorrowViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    GenericViewSet,
+):
+    queryset = Borrow.objects.select_related("book", "user")
+    serializer_class = BorrowSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        is_active = self.request.query_params.get("is_active")
+        if is_active is not None:
+            if is_active.lower() == "true":
+                queryset = queryset.filter(actual_return_date__isnull=True)
+            elif is_active.lower() == "false":
+                queryset = queryset.filter(actual_return_date__isnull=False)
+        # Не проверенно
+        if self.request.user.is_staff:
+            user_id = self.request.query_params.get("user_id")
+            if user_id:
+                queryset = queryset.filter(user_id=user_id)
+        else:
+            queryset = queryset.filter(user=self.request.user)
+
+        return queryset
+
