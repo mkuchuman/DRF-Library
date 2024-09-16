0884ab92ac23cb36349ec92c6848d6bd3d2a7abd
mkuchuman <mkuchuman1@gmail.com>
2024-09-16 22:00:31 +0200

    Urls from payments.urls included

--- books_service/urls.py
+++ books_service/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path("api/books/", include("books.urls", namespace="books")),
     path("api/users/", include("user.urls", namespace="user")),
     path("api/borrowings/", include("borrow.urls", namespace="borrowings")),
+    path("api/payments/", include("payments.urls")),
 ]
