57686e54a16e3be4b63c36127b8bd3e1a0c86c06
mkuchuman <mkuchuman1@gmail.com>
2024-09-16 21:45:57 +0200

    check_overdue_borrowings implemented

---
+++ books_service/tasks.py
@@ -0,0 +1,23 @@
+from django.utils import timezone
+
+from django_q.tasks import schedule
+
+from borrow.models import Borrow
+from telegram_helper import send_message
+
+
+def check_overdue_borrowings():
+    overdue_borrowings = Borrow.objects.filter(expected_return_date__lte=timezone.now().date(), actual_return_date=None)
+    if overdue_borrowings:
+        for borrow in overdue_borrowings:
+            message = f"The borrowing {borrow.book.title} by {borrow.user.email} is overdue"
+            send_message(message)
+    else:
+        send_message("No borrowings overdue found")
+
+
+schedule(
+     'books_service.tasks.check_overdue_borrowings',
+     schedule_type='I',  # Interval
+     minutes=1
+)
