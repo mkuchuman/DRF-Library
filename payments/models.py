efc978c28ee5f181d76af2d23bb0702ea7cc4031
mkuchuman <mkuchuman1@gmail.com>
2024-09-17 18:53:41 +0200

    Added migrations

--- payments/models.py
+++ payments/models.py
@@ -6,23 +6,19 @@ from borrow.models import Borrow
 # Create your models here.
 class Payment(models.Model):
     class PaymentStatus(models.TextChoices):
-        PENDING = 'PENDING'
-        PAID = 'PAID'
+        PENDING = "PENDING"
+        PAID = "PAID"
 
     class PaymentType(models.TextChoices):
-        PAYMENT = 'PAYMENT'
-        FINE = 'FINE'
+        PAYMENT = "PAYMENT"
+        FINE = "FINE"
 
     status = models.CharField(
-        max_length=7,
-        choices=PaymentStatus.choices,
-        default=PaymentStatus.PENDING
+        max_length=7, choices=PaymentStatus.choices, default=PaymentStatus.PENDING
     )
 
     type = models.CharField(
-        max_length=7,
-        choices=PaymentType.choices,
-        default=PaymentType.PAYMENT
+        max_length=7, choices=PaymentType.choices, default=PaymentType.PAYMENT
     )
     borrowing = models.ForeignKey(Borrow, on_delete=models.CASCADE)
     session_url = models.URLField(max_length=255)
