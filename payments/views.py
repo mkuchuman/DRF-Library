293513b5f68a2c37f4e2035bb02767432d888f6e
mkuchuman <mkuchuman1@gmail.com>
2024-09-22 20:04:04 +0200

    views for successful_payment and cancelled_payment created

--- payments/views.py
+++ payments/views.py
@@ -1,10 +1,14 @@
 from rest_framework.viewsets import GenericViewSet
 from rest_framework import mixins, status
 from rest_framework.decorators import action
+import stripe
+from django.db import transaction
+from books_service import settings
 from payments.models import Payment
 from payments.serializers import PaymentSerializer
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from django.http import HttpResponse
 
 
 class PaymentViewSet(GenericViewSet, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.ListModelMixin):
@@ -33,3 +37,22 @@ class PaymentViewSet(GenericViewSet, mixins.RetrieveModelMixin, mixins.UpdateMod
         )
         serializer = PaymentSerializer(cancelled_payments, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@transaction.atomic
+def successful_payment(request, session_id):
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    session = stripe.checkout.Session.retrieve(session_id)
+
+    if session.payment_status == "paid":
+        payment = Payment.objects.get(session_id=session_id)
+        payment.status = Payment.PaymentStatus.PAID
+        payment.save()
+
+        return HttpResponse("Payment was successfully paid")
+    else:
+        return HttpResponse("Payment was not successfully paid")
+
+
+def cancelled_payment(request):
+    return HttpResponse("Payment was canceled. You can pay later , the session is available for only 24 hours")
