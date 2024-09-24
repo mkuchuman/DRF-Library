595a32bc2532c0f150c21b4e79e5eaff5d0948a9
mkuchuman <mkuchuman1@gmail.com>
2024-09-24 14:52:17 +0200

    SECRET_KEY now in .env file

--- books_service/settings.py
+++ books_service/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-afi06_x=+gzblmhah!2wz3ew&j+boijt9r88)-9sggp4l%nxt@"
+SECRET_KEY = os.getenv("SECRET_KEY")
 STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY")
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
 
